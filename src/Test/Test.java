823acd386c41998948a2f9d4dee0f301f6c7bb19
EiseleL <luca.eisele@hak-reutte.ac.at>
2024-01-15 14:49:27 +0100

    second commit

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,9 +1,12 @@
 package Test;
 
+import java.sql.SQLOutput;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
+		System.out.println("Test");
 
 	}
 
